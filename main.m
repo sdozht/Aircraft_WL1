8e97a639a4cf1dd44323f4d0d4fc07148098bc92
Haotian Zhu <zhuht@buaa.edu.cn>
2019-04-19 19:13:16 +0800

    Yuandev (#8)
    * 加入了气动模块
    20190401
    
    * 加入新的气动模块

--- main.m
+++ main.m
@@ -5,6 +5,7 @@ Sim_Control = struct;
 Sim_Control.init = [0,0,0;... %Xe Ye Ze
                     0,0,0;... %u v w
                     0,0,0;... %roll pitch yaw
-                    0,0,0]; %p q r
+                    0,0,0 ]; %p q r
 %%
 run('Aircraft_WL1');
+ 
